bd5c58c6add4bbdb9b56ef00569fca7c049fc8a7
alpenfritz <mymailismissing@gmail.com>
2018-04-07 19:45:58 +0900

    Link postsIndex and postsShow

--- src/components/postsShow.js
+++ src/components/postsShow.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } = this.props.match.params; // provided by <Route /> from react-router
-    this.props.fetchPost(id);
+    if (!this.props.post) { // don't refresh every time
+      const { id } = this.props.match.params; // provided by <Route /> from react-router
+      this.props.fetchPost(id);
+    }
   }
 
   render() {
@@ -17,6 +20,7 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to="/">Back To Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
