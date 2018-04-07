88cad0e20cafeb8e36078fdff28c652fd26eb084
alpenfritz <mymailismissing@gmail.com>
2018-04-07 19:24:06 +0900

    Add fetchPost action creator (GET request)

--- src/components/postsShow.js
+++ src/components/postsShow.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params; // provided by <Route /> from react-router
+    this.props.fetchPost(id);
+  }
+
   render() {
+    const { post } = this.props;
+
+    if(!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
