23234b57753f6abf0ddb9d7d11f7827e1c9592e9
alpenfritz <mymailismissing@gmail.com>
2018-04-07 13:48:59 +0900

    Add postsNew logic with input validation using redux-form package

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as FormReducer } from 'redux-form';
+
 import PostsReducer from './reducerPosts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
+  form: FormReducer,
 });
 
 export default rootReducer;
