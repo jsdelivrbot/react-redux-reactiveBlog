92e6b642122b9a7e99131e86f97d3ee3bf38bd1a
alpenfritz <mymailismissing@gmail.com>
2018-04-07 20:14:59 +0900

    Add deletePost action creator

--- src/components/postsShow.js
+++ src/components/postsShow.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
-    if (!this.props.post) { // don't refresh every time
+    // if (!this.props.post) { // don't refresh every time
       const { id } = this.props.match.params; // provided by <Route /> from react-router
       this.props.fetchPost(id);
-    }
+    // }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      // PROGRAMATIC NAVIGATION: this.props.history comes from <Route /> created in /src/index.js
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -21,6 +29,12 @@ class PostsShow extends Component {
     return (
       <div>
         <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -33,4 +47,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
--- src/reducers/reducerPosts.js
+++ src/reducers/reducerPosts.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch(action.type) {
@@ -11,6 +11,9 @@ export default function(state = {}, action) {
       return { ...state, [action.payload.data.id]: action.payload.data };
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case DELETE_POST:
+      return _.omit(state, action.payload);
+      // if state were an array: return _.reject(state, post => post.id === action.payload);
     default:
       return state;
   }
